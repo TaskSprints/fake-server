b8b0cdc17b2a37a733caba27a8c0168ea4e0997a
KNU-K <knukang334@gmail.com>
2024-08-31 22:29:01 +0900

    feat : loader 구성

--- loader/index.js
+++ loader/index.js
@@ -1,7 +1,14 @@
+const expressLoader = require("./express");
+const fakeServerLoader = require("./fake-server");
+
 /**
  * Express 애플리케이션에 미들웨어 및 라우트를 추가합니다.
  *
  * @param {Object} param0 - 설정 객체
  * @param {import("express").Application} param0.app - Express 애플리케이션 인스턴스
  */
-module.exports = async ({ app }) => {};
+module.exports = async ({ app }) => {
+    const fakeServerConfig = await fakeServerLoader({ app });
+    console.log("성공적으로 fakeServer 설정을 읽어왔습니다");
+    await expressLoader({ app, fakeServerConfig });
+};
